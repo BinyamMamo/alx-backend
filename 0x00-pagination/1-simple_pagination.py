a4155a935ead8fee7dc9e380d662fdf39bae92c9
BinyamMamo <binyammamo01@gmail.com>
2024-04-01 14:10:43 +0400

    fix: add pagination to server and implement get_page method

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -4,7 +4,6 @@ Task 1. Simple pagination
 """
 from typing import List, Tuple
 import csv
-import math
 
 
 class Server:
@@ -28,13 +27,13 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-            assert isinstance(page, int), 'invalid data type'
-            assert isinstance(page_size, int), 'invalid data type'
-            assert page > 0, 'invalid page'
-            assert page_size > 0, 'invalid page size'
-            self.dataset()
-            start_index, end_index = index_range(page, page_size)
-            return self.__dataset[start_index: end_index]
+        assert isinstance(page, int), 'invalid data type'
+        assert isinstance(page_size, int), 'invalid data type'
+        assert page > 0, 'invalid page'
+        assert page_size > 0, 'invalid page size'
+        self.dataset()
+        start_index, end_index = index_range(page, page_size)
+        return self.__dataset[start_index: end_index]
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
@@ -44,8 +43,3 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return start_index, end_index
-
-
-if __name__ == "__main__":
-    s = Server()
-    s.dataset()
