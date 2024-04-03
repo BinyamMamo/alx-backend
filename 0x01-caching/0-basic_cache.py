50f7c8a67f0a70729d0c5d3d0b080f84cc2fb329
BinyamMamo <binyammamo01@gmail.com>
2024-04-03 09:58:09 +0400

    refactor: Import BaseCaching and update BasicCache class

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -4,7 +4,8 @@ Task 0. Basic dictionary
 Create a class `BasicCache` that inherits
 from `BaseCaching` and is a caching system
 """
-from base_caching import BaseCaching
+
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -12,18 +13,24 @@ class BasicCache(BaseCaching):
     BasicCache inherits from BaseCaching and implements get and put methods
     """
 
-    def get(self, key: str) -> object:
+    def __init__(self):
+        """
+        Initialize the class using the parent class __init__ method
+        """
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """
+        Add or update an item in the cache
+        """
+        if key and item:
+            self.cache_data[key] = item
+        pass
+
+    def get(self, key):
         """
         Get an item from the cache by key
         """
         if key is None:
             return None
         return self.cache_data.get(key, None)
-
-    def put(self, key: str, item: object) -> None:
-        """
-        Add or update an item in the cache
-        """
-        if key and item:
-            self.cache_data.update({key: item})
-        pass
