a34360301fa8e0cdda3c97a4b2220c85118a4678
BinyamMamo <binyammamo01@gmail.com>
2024-04-09 13:31:42 +0400

    feat: Add basic Flask app in 0-app.py

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+0. Basic Flask app
+setup a basic Flask app in 0-app.py
+"""
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=['GET'], strict_slashes=False)
+def home():
+    """
+    renders the home page
+    """
+    return render_template("index.html")
